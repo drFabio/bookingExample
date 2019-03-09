93100261d36a895a67a16fd933192a40775a9d95
fabio <fabiocostadev@gmail.com>
2019-03-09 14:48:30 +0100

    tests: added tests descriptors

---
+++ api/tests/data/BookingManager.test.js
@@ -0,0 +1,6 @@
+describe('BookingManager', () => {
+  it.todo('Books an unbooked property');
+  it.todo("Can't book a booked property");
+  it.todo('Can cancel a booking');
+  it.todo("Can't book if the capacity is below the booking ");
+});
---
+++ api/tests/data/UserManager.test.js
@@ -0,0 +1,6 @@
+describe('UserManager', () => {
+  it.todo('Can login a user');
+  it.todo('Can logout a user');
+  it.todo('Can signUp a user');
+  it.todo("Can't signup a user with the same email");
+});
