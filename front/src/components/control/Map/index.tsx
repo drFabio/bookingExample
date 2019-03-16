5b25ba13ce7d4dd5bba8c7d3e3a49f6ba5de41c5
fabio <fabiocostadev@gmail.com>
2019-03-16 23:47:22 +0100

    new: refactored maps and added markers

--- front/src/components/control/Map/index.tsx
+++ front/src/components/control/Map/index.tsx
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect, Fragment } from "react";
-import L, { LatLngExpression } from "leaflet";
+import React, { useRef, useEffect, Fragment, MutableRefObject } from "react";
+import L, { Map as LeafletMap, LayerGroup, Marker } from "leaflet";
 import styled from "styled-components";
+import { LatLng } from "../../../types";
 
 const MapContainer = styled.section`
   min-height: 10vh;
@@ -9,23 +10,57 @@ const MapContainer = styled.section`
 `;
 
 export interface MapProps {
-  position: [number, number];
+  position?: null | LatLng;
+  markers?: null | Array<LatLng>;
 }
-export function Map({ position }: MapProps) {
-  const mapRef = useRef<HTMLElement>(null);
+export function Map({ position, markers }: MapProps) {
+  if (!position) {
+    return null;
+  }
+  const mapContainerRef = useRef<HTMLElement>(null);
+  const mapRef: MutableRefObject<LeafletMap | undefined> = useRef<LeafletMap>();
   useEffect(() => {
-    if (mapRef && mapRef.current) {
-      const map = L.map(mapRef.current as HTMLElement).setView(position, 13);
+    if (mapContainerRef.current) {
+      mapRef.current = L.map(mapContainerRef.current).setView(position, 13);
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
-        map
+        mapRef.current
       );
-      L.marker(position).addTo(map);
     }
   }, []);
+  const positionRef: MutableRefObject<Marker<any> | undefined> = useRef<
+    Marker<any>
+  >();
+  useEffect(() => {
+    if (positionRef.current) {
+      positionRef.current.setLatLng(position);
+    } else {
+      positionRef.current = L.marker(position)
+        .bindPopup("We think you are here")
+        .addTo(mapRef.current as LeafletMap);
+    }
+  }, [position]);
+
+  const layerRef: MutableRefObject<LayerGroup<any> | undefined> = useRef<
+    LayerGroup<any>
+  >();
+  useEffect(() => {
+    layerRef.current = L.layerGroup().addTo(mapRef.current as LeafletMap);
+  }, []);
+
+  useEffect(() => {
+    layerRef.current!.clearLayers();
+    if (markers) {
+      markers.forEach(marker => {
+        L.marker(marker)
+          .bindPopup(JSON.stringify(marker))
+          .addTo(layerRef.current as LayerGroup);
+      });
+    }
+  }, [markers]);
 
   return (
     <Fragment>
-      <MapContainer ref={mapRef} />
+      <MapContainer ref={mapContainerRef} />
     </Fragment>
   );
 }
